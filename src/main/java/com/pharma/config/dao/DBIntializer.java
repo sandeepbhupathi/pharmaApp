f2e4ddc7ad1038917cc7f6ebf265eca6381a39d2
sandeepbupathi08 <sandeepbupathi08@gmail.com>
2018-09-11 19:25:44 +0530

    removed JDBC

--- src/main/java/com/pharma/config/dao/DBIntializer.java
+++
@@ -1,8 +0,0 @@
-package com.pharma.config.dao;
-
-public interface DBIntializer {
-String DRIVER="oracle.jdbc.driver.OracleDriver";
-String CON_STRING="jdbc:oracle:thin:@localhost:1521:orcl";
-String USERNAME="pharmacy";
-String PASSWORD="pharmacy";
-}
