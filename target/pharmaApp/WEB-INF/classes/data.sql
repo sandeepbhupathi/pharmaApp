22c4a8ab0f759e7f25f309eb58585104c6f1507a
sandeepbupathi08 <sandeepbupathi08@gmail.com>
2018-09-12 21:32:03 +0530

    added unit tests

--- target/pharmaApp/WEB-INF/classes/data.sql
+++
@@ -1,1 +0,0 @@
-insert into PHARMACYADMIN (username,password) values ('sandeep','sandeep');
--- target/pharmaApp/WEB-INF/classes/schema.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE IF EXISTS PHARMACYADMIN;
-create table PHARMACYADMIN (id INTEGER IDENTITY(1,1) PRIMARY KEY,username varchar(100),password varchar(100));
